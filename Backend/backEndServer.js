6e5a30331e516684ad9a9a6f67c51b1fc143edb8
daleyusuf23 <daleyusuf23@gmail.com>
2022-10-11 07:57:43 -0500

    ROUTES working

--- Backend/backEndServer.js
+++ Backend/backEndServer.js
@@ -14,18 +14,15 @@ const client = new Client({
 //     password: 'docker',
 //     database: 'fanbase'
 });
-
-
 client.connect();
 
-
+//TEST
 app.get('/', (req,res) => {
     res.status(200).send('Hello world!');
 });
 
-
+//GET ROUTE
 app.get('/api/fan', (req,res) => {
-    // res.status(200).send('Hello world!');
     client.query('SELECT * FROM fan', (err, result) => {
         if (err) {
             console.error(err)
@@ -33,8 +30,46 @@ app.get('/api/fan', (req,res) => {
             res.send(result.rows);
         }
     })
-})
+});
 
+//POST ROUTE
+app.post('/api/fan', (req,res) => {
+    let {name, phone, tier} = req.body;
+    client.query ('INSERT INTO fan (name, phone, tier) VALUES ($1, $2, $3);', [name, phone, tier], (error, results) => {
+        if (error) {
+            console.log(error)
+        } else {
+            res.send(results.rows);
+        }
+     })
+     console.log(name, phone, tier);
+     res.status(200);
+});
+
+//PATCH ROUTE
+app.patch('/api/fan/:id', (req,res) => {
+    client.query('UPDATE fan SET name = $1 WHERE fan_id = $2', [req.body.name, req.params.id], (error, results) => {
+        if(error) {
+            console.error(error)
+        } else {
+             res.send(results.rows);
+        }
+    })
+});
+
+//DELETE ROUTE
+app.delete('/api/fan/:id', (req,res) => {
+    client.query('DELETE FROM fan WHERE fan_id = $1', [req.params.id], (error, results) => {
+        if(error) {
+            console.error(error)
+        } else {
+            res.send (results.rows);
+        }
+    })
+});
+
+
+//PORT
 app.listen(PORT, () => {
     console.log(`Running on ${PORT}`)
    });
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # fanpageMVP
+Hands-on: Full Stack - Single Page Application Rep
+
+In this Full stack website will comprise of all three components to create a website
+that tracks an individuals orders at a restaurant.
+
+
+![placeholder-text](Untitled-Diagram.jpg)
+
+![placeholder-text](erd.png)
